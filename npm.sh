4609fb89fddc4ffb111bc5f405cefcb51ad111c4
Rizky Syazuli <br4inwash3r@gmail.com>
2016-06-15 10:39:40 +0700

    update npm installation

--- npm.sh
+++ npm.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
-# Install npm packages
+# Essentials
 npm install -g bower
 npm install -g grunt-cli
 npm install -g gulp-cli
+npm install -g less
 npm install -g webpack
 npm install -g yo
 npm install -g browser-sync
 npm install -g phantomjs
+npm install -g coffee-script
+npm install -g adonis-cli
+npm install -g nodemon
+
+# Linters
+npm install -g csslint
+npm install -g coffeelint
+npm install -g htmlhint
+npm install -g jsonlint
+npm install -g jshint
+npm install -g sass-lint
+npm install -g yaml-js
