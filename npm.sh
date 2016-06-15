1fc09dadd76a47a1dc6b5c27da5f8d4e2db14208
Rizky Syazuli <br4inwash3r@gmail.com>
2016-06-15 09:48:38 +0700

    add npm package installer

---
+++ npm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install npm packages
+npm install -g bower
+npm install -g grunt-cli
+npm install -g gulp-cli
+npm install -g webpack
+npm install -g yo
+npm install -g browser-sync
+npm install -g phantomjs
